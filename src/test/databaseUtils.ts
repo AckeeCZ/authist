126335e65b66ca09a3d35be6cb09765f52f60b5f
Stefan Prokop <sprokop1010@seznam.cz>
2020-08-07 15:20:31 +0200

    ✅ Add usernamePassword tests

---
+++ src/test/databaseUtils.ts
@@ -0,0 +1,27 @@
+import { DataTypes, Sequelize } from 'sequelize';
+
+const sqlite = new Sequelize('sqlite::memory:');
+
+// tslint:disable-next-line:variable-name
+const User = sqlite.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    name: {
+        type: DataTypes.STRING,
+    },
+});
+
+export const initDb = async () => {
+    return await User.sync();
+};
---
+++ src/test/integration/usernamePassword.test.ts
@@ -0,0 +1,89 @@
+import { createAuthenticator, ERROR_CODE, User } from '../../lib';
+import { initDb } from '../databaseUtils';
+
+let userModel: any;
+
+describe('UsernamePasswordProvider', () => {
+    const username = 'tester';
+    const password = '***';
+    beforeAll(async () => {
+        userModel = await initDb();
+    });
+    afterAll(async () => {
+        await userModel.dropAllSchemas();
+    });
+    test('Can auto-register user', async () => {
+        const authenticator = createAuthenticator({
+            getUserById,
+            usernamePassword: {
+                getUserByUsername,
+                saveNonExistingUser: (data, hashedPassword, _req) =>
+                    userModel.create({ username: data.email, password: hashedPassword }),
+            },
+        });
+        const { user, credentials } = await authenticator.signInWithUsernameAndPassword(username, password);
+        expect(user).not.toHaveProperty('password');
+        expect(user.email).toBe(username);
+        expect(user).toHaveProperty('uid');
+        expect(Object.keys(credentials).sort()).toEqual(
+            ['accessToken', 'refreshToken', 'expiresIn', 'refreshExpiresIn'].sort()
+        );
+    });
+    test('User can sign-in', async () => {
+        const authenticator = createAuthenticator({
+            getUserById,
+            usernamePassword: {
+                getUserByUsername,
+            },
+        });
+        const { user, credentials } = await authenticator.signInWithUsernameAndPassword(username, password);
+        expect(user).not.toHaveProperty('password');
+        expect(user.email).toBe(username);
+        expect(user).toHaveProperty('uid');
+        expect(Object.keys(credentials).sort()).toEqual(
+            ['accessToken', 'refreshToken', 'expiresIn', 'refreshExpiresIn'].sort()
+        );
+    });
+    test('Non-existing user cannot sign-in', async () => {
+        const authenticator = createAuthenticator({
+            usernamePassword: {
+                getUserByUsername,
+            },
+        });
+        try {
+            await authenticator.signInWithUsernameAndPassword('username', 'password');
+        } catch (error) {
+            expect(error.errorCode).toMatch((ERROR_CODE.UserNotFound as any).code);
+            return;
+        }
+        throw Error();
+    });
+});
+
+const getUserByUsername = async (username: string) => {
+    const user = await userModel.findOne({ where: { username } });
+    if (!user) {
+        return;
+    }
+    const authistUser: User & { password: string } = {
+        password: user.password,
+        email: user.username,
+        providerData: {},
+        uid: String(user.id),
+    };
+    return authistUser;
+};
+
+const getUserById = async (uid: string) => {
+    const user = await userModel.findOne({ where: { id: uid } });
+    if (!user) {
+        return;
+    }
+    const authistUser: User & { password: string } = {
+        password: user.password,
+        email: user.username,
+        providerData: {},
+        uid: String(user.id),
+    };
+    return authistUser;
+};

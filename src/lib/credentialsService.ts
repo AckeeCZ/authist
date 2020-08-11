5f756ac11cf41fd6ff0d6e32405d13d293f6609f
Stefan Prokop <sprokop1010@seznam.cz>
2020-08-11 11:47:32 +0200

    🔒 Update JWT token payload

--- src/lib/credentialsService.ts
+++ src/lib/credentialsService.ts
@@ -1,24 +1,39 @@
 import { sign, verify } from 'jsonwebtoken';
 import { AuthistOptions, Credentials, User } from './authist';
+import { ERROR_CODE, NotAuthenticated } from './error';
 import { handleError } from './providerUtils';
 
-export const createCredentials = (user: User, options: AuthistOptions): Credentials => ({
-    accessToken: sign({ uid: user.uid }, getSecret(options), {
+export const createCredentials = (user: User, options: AuthistOptions): Credentials => {
+    const getPayload = (tokenType: string) => ({
+        user: {
+            uid: user.uid,
+        },
+        type: tokenType,
+    });
+    return {
+        accessToken: sign(getPayload('accessToken'), getSecret(options), {
+            expiresIn: (options.token?.lifetimeMinutes || 60) * 60,
+        }),
+        refreshToken: sign(getPayload('refreshToken'), getSecret(options), {
+            expiresIn: (options.token?.lifetimeMinutes || 60) * 60,
+        }),
         expiresIn: (options.token?.lifetimeMinutes || 60) * 60,
-    }),
-    // todo
-    refreshToken: sign({ uid: user.uid }, getSecret(options), {
-        expiresIn: (options.token?.lifetimeMinutes || 60) * 60,
-    }),
-    expiresIn: (options.token?.lifetimeMinutes || 60) * 60,
-    // todo
-    refreshExpiresIn: (options.token?.refreshLifetimeMinutes || 60) * 60,
-});
+        refreshExpiresIn: (options.token?.refreshLifetimeMinutes || 60 * 24 * 60) * 60,
+    };
+};
 
-export const verifyToken = (token: string, options: AuthistOptions) => {
+export const verifyToken = async (token: string, options: AuthistOptions) => {
     try {
+        if (!options.getUserById) {
+            throw new Error('Function `getUserById` is not implemented!');
+        }
         const secret = getSecret(options);
-        return verify(token, secret);
+        const decodedToken = verify(token, secret) as { user: { uid: string } };
+        const { uid } = decodedToken.user;
+        const user = await options.getUserById(uid);
+        if (!user) {
+            throw new NotAuthenticated(ERROR_CODE.UserNotFound);
+        }
     } catch (error) {
         return handleError(error, {}, options);
     }

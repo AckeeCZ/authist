dba89a72f7daa295f5e6350f7e2fe4333b037714
Stefan Prokop <sprokop1010@seznam.cz>
2020-12-14 14:57:55 +0100

    ✅ Add bearer MW tests

---
+++ src/test/integration/bearer.test.ts
@@ -0,0 +1,7 @@
+describe('Bearer authentication middleware', () => {
+    test.todo('Unauthorized return an error');
+    test.todo('Unsupported type return an error');
+    test.todo('Invalid token return an error');
+    test.todo('Invalid token return custom error');
+    test.todo('Can authorize user');
+});

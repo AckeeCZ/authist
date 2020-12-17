eb64af15b4a74a51eef8c781e772a20e8c1b1385
Stefan Prokop <sprokop1010@seznam.cz>
2020-12-17 11:52:51 +0100

    ✨ Add bearer mw

--- src/lib/authMiddleware.ts
+++ src/lib/authMiddleware.ts
@@ -1,27 +1,42 @@
-import { IncomingMessage, ServerResponse } from 'http';
-import { AuthistOptions } from './authist';
+import { NextFunction, Request, Response } from 'express';
+import { AuthistOptions, User } from './authist';
 import { verifyToken } from './credentialsService';
 import { ERROR_CODE, NotAuthenticated } from './error';
 
-export const bearer = (options: AuthistOptions) => async (request: IncomingMessage, response: ServerResponse) => {
-    const authorization = request.headers.authorization;
+export type BearerError = Error & { status?: number; statusCode?: number; toJSON?: any };
+
+export const bearer = async (
+    options: AuthistOptions,
+    authorization?: string
+): Promise<[BearerError | undefined, User | undefined]> => {
     if (!authorization) {
-        const error = new NotAuthenticated(ERROR_CODE.AuthenticationRequired);
-        response.statusCode = error.status;
-        return response.end(error.toJSON());
+        return [new NotAuthenticated(ERROR_CODE.AuthenticationRequired), undefined];
     }
     const [bearer, accessToken] = authorization.split(' ');
     if (!bearer || bearer.toLowerCase() !== 'bearer') {
-        const error = new NotAuthenticated(ERROR_CODE.UnsupportedAuthorization);
-        response.statusCode = error.status;
-        return response.end(error.toJSON());
+        return [new NotAuthenticated(ERROR_CODE.UnsupportedAuthorization), undefined];
     }
     try {
-        // @ts-ignore
-        request.user = await verifyToken(accessToken, options);
-        request.push(null);
+        const user = await verifyToken(accessToken, options);
+        return [undefined, user];
     } catch (error) {
-        response.statusCode = error.status ?? error.statusCode ?? 401;
-        return response.end(error.toJSON ? error.toJSON() : JSON.stringify(error));
+        return [error, undefined];
     }
+};
+
+export const expressBearer = (options: AuthistOptions) => async (
+    request: Request & { user?: User },
+    response: Response,
+    next: NextFunction
+) => {
+    const [error, user] = await bearer(options, request.headers.authorization);
+    if (error) {
+        if (options.onExpressAuthenticationFailure) {
+            return options.onExpressAuthenticationFailure(error, request, response, next);
+        }
+        response.statusCode = error.status ?? error.statusCode ?? 401;
+        return response.json({ message: error?.message });
+    }
+    request.user = user;
+    next();
 };
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,25 @@
-export * from './authist';
-export { ERROR_CODE } from './error';
-export { verifyToken } from './credentialsService';
+export {
+    Authenticator,
+    AuthistOptions,
+    Credentials,
+    createAuthenticator,
+    TokenOptions,
+    User,
+    UserCredentials,
+    UserInfo,
+    UserMetadata,
+} from './authist';
+export { ERROR_CODE, ErrorCode, NotAuthenticated } from './error';
+export { BearerError, bearer, expressBearer } from './authMiddleware';
+export { verifyToken, createCredentials } from './credentialsService';
+export {
+    EmailPasswordProviderOptions,
+    getHashingAlgorithm,
+    HashingAlgorithm,
+    saveNonExistingUser,
+    sendRegistrationEmail,
+    signInWithEmailAndPassword,
+    validatePassword,
+    validateSignIn,
+    validateUser,
+} from './providers/emailPasswordProvider';

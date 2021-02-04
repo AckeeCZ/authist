320fa4b5df4b60720b822e0774ba663fbd1777c4
Stefan Prokop <sprokop1010@seznam.cz>
2021-02-04 12:17:32 +0100

    ✨ Add reset & change password express m

--- src/lib/authMiddleware.ts
+++ src/lib/authMiddleware.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import { AuthistOptions, Credentials, User } from './authist';
 import { refreshToken as refreshUserToken, verifyToken } from './credentialsService';
 import { ERROR_CODE, NotAuthenticated } from './error';
+import { changePassword as changeUserPassword, resetPassword as resetUserPassword } from './resetPasswordService';
 
 export type BearerError = Error & { status?: number; statusCode?: number; toJSON?: any };
 
@@ -39,6 +40,39 @@ const refreshToken = async (
     }
 };
 
+const resetPassword = async (
+    options: AuthistOptions,
+    email?: string
+): Promise<[BearerError | undefined, { token: any } | undefined]> => {
+    if (!email) {
+        return [new NotAuthenticated(ERROR_CODE.EmailRequired), undefined];
+    }
+    try {
+        const token = await resetUserPassword(options)(email);
+        return [undefined, token];
+    } catch (error) {
+        return [error, undefined];
+    }
+};
+
+const changePassword = async (
+    options: AuthistOptions,
+    data?: { token?: any; password?: string }
+): Promise<[BearerError | undefined, undefined]> => {
+    if (!data?.token) {
+        return [new NotAuthenticated(ERROR_CODE.ResetPasswordTokenRequired), undefined];
+    }
+    if (!data?.password) {
+        return [new NotAuthenticated(ERROR_CODE.PasswordRequired), undefined];
+    }
+    try {
+        await changeUserPassword(options)(data.token, data.password);
+        return [undefined, undefined];
+    } catch (error) {
+        return [error, undefined];
+    }
+};
+
 export const expressBearer = (options: AuthistOptions) => async (
     request: Request & { user?: User },
     response: Response,
@@ -70,4 +104,37 @@ export const expressRefreshToken = (options: AuthistOptions) => async (
         return response.json({ message: error?.message });
     }
     return response.json(credentials);
+};
+
+export const expressResetPassword = (options: AuthistOptions) => async (
+    request: Request,
+    response: Response,
+    next: NextFunction
+) => {
+    const [error, token] = await resetPassword(options, request.body?.email ?? request.query.email);
+    if (error) {
+        if (options.onExpressAuthenticationFailure) {
+            return options.onExpressAuthenticationFailure(error, request, response, next);
+        }
+        response.statusCode = error.status ?? error.statusCode ?? 400;
+        return response.json({ message: error?.message });
+    }
+    return response.json(token);
+};
+
+export const expressChangePassword = (options: AuthistOptions) => async (
+    request: Request,
+    response: Response,
+    next: NextFunction
+) => {
+    const [error] = await changePassword(options, { ...request.body, ...request.query });
+    if (error) {
+        if (options.onExpressAuthenticationFailure) {
+            return options.onExpressAuthenticationFailure(error, request, response, next);
+        }
+        response.statusCode = error.status ?? error.statusCode ?? 400;
+        return response.json({ message: error?.message });
+    }
+    response.statusCode = 204;
+    return response.send();
 };
